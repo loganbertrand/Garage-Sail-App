8db24c60db934f15893b73d659f8da56812ed8f4
Logan Bertrand <loganwbertrand@gmail.com>
2019-06-14 11:37:51 -0700

    Linting and Travis added to the project

--- public/js/index.js
+++ public/js/index.js
@@ -3,120 +3,121 @@ var $name = $('#formName');
 var $address = $('#formAddress');
 var $submitBtn = $('#submit');
 var $time = $('#formTime');
+// eslint-disable-next-line no-unused-vars
 var $categories = $('#formCategories');
 var $image = $('#formImage');
 
-var testList = $('#testList');
+var $testList = $('#testList');
 var deleteButton = $('#delete-button');
 
 // The API object contains methods for each kind of request we'll make
 var API = {
-    saveGarageSale: function(garagesale) {
-        console.log('attempting to post data');
-        return $.ajax({
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            type: 'POST',
-            url: 'api/garagesale/',
-            data: JSON.stringify(garagesale)
-        });
-    },
-    getGarageSales: function() {
-        return $.ajax({
-            url: 'api/garagesale/',
-            type: 'GET'
-        });
-    },
-    deleteGarageSale: function(id) {
-        return $.ajax({
-            url: 'api/garagesale/' + id,
-            type: 'DELETE'
-        });
-    }
+  saveGarageSale: function(garagesale) {
+    console.log('attempting to post data');
+    return $.ajax({
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      type: 'POST',
+      url: 'api/garagesale/',
+      data: JSON.stringify(garagesale)
+    });
+  },
+  getGarageSales: function() {
+    return $.ajax({
+      url: 'api/garagesale/',
+      type: 'GET'
+    });
+  },
+  deleteGarageSale: function(id) {
+    return $.ajax({
+      url: 'api/garagesale/' + id,
+      type: 'DELETE'
+    });
+  }
 };
 
 // refreshExamples gets new examples from the db and repopulates the list
 var refreshExamples = function() {
-    API.getGarageSales().then(function(data) {
-        var $Sales = data.map(function(garagesale) {
-            var $a = $('<a>')
-                .text(garagesale.text)
-                .attr('href', '/garagesale/' + garagesale.id);
+  API.getGarageSales().then(function(data) {
+    var $Sales = data.map(function(garagesale) {
+      var $a = $('<a>')
+        .text(garagesale.text)
+        .attr('href', '/garagesale/' + garagesale.id);
 
-            var $li = $('<li>')
-                .attr({
-                    class: 'list-group-item',
-                    'data-id': garagesale.id
-                })
-                .append($a);
+      var $li = $('<li>')
+        .attr({
+          class: 'list-group-item',
+          'data-id': garagesale.id
+        })
+        .append($a);
 
-            var $button = $('<button>')
-                .addClass('btn btn-danger float-right delete')
-                .text('ｘ');
+      var $button = $('<button>')
+        .addClass('btn btn-danger float-right delete')
+        .text('ｘ');
 
-            $li.append($button);
+      $li.append($button);
 
-            return $li;
-        });
-
-        $testList.empty();
-        $testList.append($Sales);
+      return $li;
     });
+
+    $testList.empty();
+    $testList.append($Sales);
+  });
 };
 
 // handleFormSubmit is called whenever we submit a new example
 // Save the new example to the db and refresh the list
 var handleFormSubmit = function(event) {
-    event.preventDefault();
-    console.log('Submit button was clicked');
-    var formData = {
-        name: $name.val().trim(),
-        address: $address.val().trim(),
-        time: $time.val().trim(),
-        //categories: $categories.val().trim(),
-        image: $image.val().trim()
-    };
-    console.log(formData);
-    if (
-        !(
-            formData.name &&
-            formData.address &&
-            formData.time && //&& formData.categories
-            formData.image
-        )
-    ) {
-        alert('You must enter information for all parts of the form!');
-        return;
-    }
+  event.preventDefault();
+  console.log('Submit button was clicked');
+  var formData = {
+    name: $name.val().trim(),
+    address: $address.val().trim(),
+    time: $time.val().trim(),
+    //categories: $categories.val().trim(),
+    image: $image.val().trim()
+  };
+  console.log(formData);
+  if (
+    !(
+      formData.name &&
+      formData.address &&
+      formData.time && //&& formData.categories
+      formData.image
+    )
+  ) {
+    alert('You must enter information for all parts of the form!');
+    return;
+  }
 
-    API.saveGarageSale(formData).then(function() {
-        console.log('posted successfully? Time to Get');
-        refreshExamples();
-    });
+  API.saveGarageSale(formData).then(function() {
+    console.log('posted successfully? Time to Get');
+    refreshExamples();
+  });
 
-    //$exampleText.val("");
-    //$exampleDescription.val("");
+  //$exampleText.val("");
+  //$exampleDescription.val("");
 
-    //clear form
-    $('.form-group input').val('');
+  //clear form
+  $('.form-group input').val('');
 };
 
 // handleDeleteBtnClick is called when an example's delete button is clicked
 // Remove the example from the db and refresh the list
 var handleDeleteBtnClick = function() {
-    var idToDelete = $(this)
-        .parent()
-        .attr('data-id');
+  var idToDelete = $(this)
+    .parent()
+    .attr('data-id');
 
-    API.deleteGarageSale(idToDelete).then(function() {
-        refreshExamples();
-    });
+  API.deleteGarageSale(idToDelete).then(function() {
+    refreshExamples();
+  });
 
-    //remove posted item
-    $(event.target)
-        .closest('.list-group-item')
-        .remove();
+  //remove posted item
+  $(event.target)
+    .closest('.list-group-item')
+    .remove();
 };
 
 // Add event listeners to the submit and delete buttons
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,26 @@
-var db = require("../models");
+var db = require('../models');
 
 module.exports = function(app) {
   // Get all examples
-  app.get("/api/garagesale", function(req, res) {
+  app.get('/api/garagesale', function(req, res) {
     db.GarageSale.findAll({}).then(function(dbGarageSales) {
       res.json(dbGarageSales);
     });
   });
 
   // Create a new GarageSale
-  app.post("/api/garagesale", function(req, res) {
-    console.log(req.body)
+  app.post('/api/garagesale', function(req, res) {
+    console.log(req.body);
     db.GarageSale.create(req.body).then(function(dbGarageSale) {
       res.json(dbGarageSale);
     });
   });
 
   // Delete an GarageSale by id
-  app.delete("/api/garagesale/:id", function(req, res) {
-    db.GarageSale.destroy({ where: { id: req.params.id } }).then(function(dbGarageSale) {
+  app.delete('/api/garagesale/:id', function(req, res) {
+    db.GarageSale.destroy({ where: { id: req.params.id } }).then(function(
+      dbGarageSale
+    ) {
       res.json(dbGarageSale);
     });
   });
